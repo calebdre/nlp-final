19ed48eda189444d7fe44e858c9e7b55a220d431
Caleb Lewis <caleb_dre@yahoo.com>
2018-12-07 23:00:11 -0500

    Added extraction example

--- lang_pair.py
+++ lang_pair.py
@@ -1,4 +1,5 @@
 import random 
+import torch
 
 class LangPair:
     def __init__(self, lang1, eos_idx1, lang2, eos_idx2):
@@ -11,9 +12,12 @@ class LangPair:
         self.data_len = len(lang1)
     
     def get_sentence(self, n):
+        s1 = self.lang1[n] + [self.eos1]
+        s2 = self.lang2[n] + [self.eos2]
+        
         return (
-            self.lang1[n] + [self.eos1],
-            self.lang2[n] + [self.eos2],
+            torch.tensor(s1, dtype=torch.long).view(-1, 1),
+            torch.tensor(s2, dtype=torch.long).view(-1, 1)
         )
     
     def get_rand_sentence(self):
